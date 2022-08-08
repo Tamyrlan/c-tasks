1d1ed8816a1188d3023884dd8bcb5ee13b13f4ce
Tamyrlan <tshurenov@gmail.com>
2022-08-08 10:55:45 +0600

    homework task2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.Write($"Число {number1} больше числа {number2}");
+} 
+else{
+    Console.Write($"Число {number1} меньше числа {number2}");
+}
