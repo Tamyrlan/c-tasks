7dd78c8bc49920ebc125dccfde0c08af6602addc
Tamyrlan <tshurenov@gmail.com>
2022-08-10 09:34:07 +0600

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число:");
+long num = Convert.ToInt64(Console.ReadLine());
+if (num %2 ==0)
+{
+    Console.Write($"Число {num} четное");
+}
+else
+{
+    Console.Write($"Число {num} не четное");
+}
