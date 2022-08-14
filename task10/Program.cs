16e043e84319f6e0cdc13c4d36cb7acc7e3be312
Tamyrlan <tshurenov@gmail.com>
2022-08-14 14:30:28 +0600

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int ShowSecondDigit (int number)
+{
+    int FirstDigit = number / 100;
+    int SecondDigit = number / 10 % 10;
+    return SecondDigit;
+}
+int showSecondDigit = ShowSecondDigit(number);
+if (number>100 && number < 1000)
+{
+    Console.WriteLine($"Второе число трехзначного числа = {showSecondDigit}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
