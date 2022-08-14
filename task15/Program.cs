7a1d0f104acd507abf186df4737c2431151859a4
Tamyrlan <tshurenov@gmail.com>
2022-08-14 18:04:16 +0600

    homework task15

---
+++ task15/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter the number of the day: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 == 6 || n % 7 == 0) Console.WriteLine("Congrats, today is day off!!");
+else Console.WriteLine ("Work hard try hard!");
