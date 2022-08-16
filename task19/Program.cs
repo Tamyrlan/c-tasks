df95f2a907e122f23b7f63944aeac5963a358aca
Tamyrlan <tshurenov@gmail.com>
2022-08-16 16:13:18 +0600

    task 19

---
+++ task19/Program.cs
@@ -0,0 +1,12 @@
+﻿int n;
+Console.Write("Введите пятизначное число: ");
+n = Convert.ToInt32(Console.ReadLine());
+if (n <= 9999 || n >= 1000000)
+{
+    Console.WriteLine("Неверное число, пожалуиста введите пятизначное число!");
+}
+if (n % 10 == n / 10000 && (n % 100) / 10 == (n / 1000) % 10)
+{
+    Console.WriteLine("Да, это палиндром!");
+}
+else Console.WriteLine("Нет, это не палиндром");
