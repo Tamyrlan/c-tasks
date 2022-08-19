81c674165395cc6b3034eb6614a12dc5504e6fc6
Tamyrlan <tshurenov@gmail.com>
2022-08-19 14:15:17 +0600

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+int x = a;
+for (int i=1; i < b; i++)
+{
+    x = x*a;
+}
+
+Console.WriteLine($"Первое число в степени второго числа =>{x}");
