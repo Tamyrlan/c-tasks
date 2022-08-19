a396b74c25d4ddac092aab59643f0a7f51981bd9
Tamyrlan <tshurenov@gmail.com>
2022-08-19 14:37:56 +0600

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random(); 
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max+1);
+    }
+    return array;
+}
+void PrintArr(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+int [] arr = CreateArrayRndInt(8,1,100);
+PrintArr(arr);
