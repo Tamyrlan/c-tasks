69d22a0ed101f9754af4834bfa7cfa9887dad8f9
Tamyrlan <tshurenov@gmail.com>
2022-08-22 23:33:11 +0600

    task39

---
+++ task39/Program.cs
@@ -0,0 +1,52 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArr(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.WriteLine("]");
+}
+
+void ReverseArray(int[] array)
+{
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size - 1;
+    while (index1 < index2)
+    {
+        int obj = array[index1];
+        array[index1] = array[index2];
+        array[index2] = obj;
+        index1++;
+        index2--;
+    }
+}
+void ReverseArray1(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size/2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+
+
+    }
+}
+int[] arr = CreateArrayRndInt(12, 1, 99);
+PrintArr(arr);
+//ReverseArray(arr);
+ReverseArray1(arr);
+PrintArr(arr);
