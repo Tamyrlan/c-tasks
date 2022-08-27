38e8e1a1d225ccc42498e424889b1aba754cfcd5
Tamyrlan <tshurenov@gmail.com>
2022-08-27 18:56:47 +0600

    modified task44

--- task44/Program.cs
+++ task44/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Fibonacci(int number)
+{
+    int fib1 = 0;
+    int fib2 = 1;
+    Console.Write($"{fib1} {fib2} ");
+    for (int i = 2; i < number; i++)
+    {
+        int nextFib = fib1+fib2;
+        Console.Write($"{nextFib} ");
+        fib1=fib2;
+        fib2=nextFib;
+    }
+}
+
+Fibonacci(n);
