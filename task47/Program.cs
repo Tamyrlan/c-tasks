59b38562ca15967a321faeec90a6cc3f73b4b753
Tamyrlan <tshurenov@gmail.com>
2022-08-27 19:09:00 +0600

    homework task47

--- task47/Program.cs
+++ task47/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    double[,] matrix = new double[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // col
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * (max - min) + min, 2);
+        }
+
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}," + " ");
+            else Console.Write($"{matrix[i, j],4}" + " ");
+        }
+        Console.WriteLine("]");
+
+    }
+}
+double[,] matr = CreateMatrixRndInt(4, 3, -10, 10);
+PrintMatrix(matr);
