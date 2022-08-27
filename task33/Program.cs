6847e88542f24cff278d3eb3ba2244ef493d97ce
Tamyrlan <tshurenov@gmail.com>
2022-08-27 18:57:17 +0600

    modified task33

--- task33/Program.cs
+++ task33/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random(); 
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max+1);
+    }
+    return array;
+}
+void PrintArr(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+bool FindElemInArray(int [] array,int n )
+{
+    bool ret = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]!= elemF)
+        {
+            ret = true;
+            break;
+        } 
+    }
+    return ret;
+}
+Console.WriteLine("Enter your number:");
+int findI = Convert.ToInt32(Console.Readline());
