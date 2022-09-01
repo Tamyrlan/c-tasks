6dc8070298946f2dbdf8986f998c13fca9c0ee09
Tamyrlan <tshurenov@gmail.com>
2022-09-01 23:18:47 +0600

    task57

---
+++ task57/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // col
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+
+    }
+}
+int[] ConvertMatrix2ArrayAndSort(int[,] matrix)
+{
+    int[] result = new int[matrix.Length];
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            result[count] = matrix[i, j];
+            count++;
+        }
+    }
+    Array.Sort(result);
+    return result;
+}
+
+void CountSimularElems(int[] array)
+{
+    int count = 1;
+    int number = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] == number) count++;
+        else
+        {
+            Console.WriteLine($"{number} -> {count}");
+            number = array[i];
+            count = 1;
+        }
+        if (i == array.Length-1)
+        {
+            Console.WriteLine($"{number} -> {count}");
+        }
+    }
+}
