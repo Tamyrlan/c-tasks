63d9786e1d7fbb208194fae79ea10192ff535eed
Tamyrlan <tshurenov@gmail.com>
2022-09-01 23:19:18 +0600

    task63

---
+++ task63/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое положительное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+void NaturalNumber(int number)
+{
+    if (number == 0) return;
+    NaturalNumber(number -1);
+    Console.Write($"{number} ");
+}
+NaturalNumber(num);
