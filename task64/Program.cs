6d68ff6177e709b6dc1b7a058a4fdc3232e55b1d
Tamyrlan <tshurenov@gmail.com>
2022-09-02 17:34:47 +0600

    task64 hw

---
+++ task64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое положительное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+void NaturalNumber(int number)
+{
+    if (number == 0) return;
+    Console.Write($"{number},");
+    NaturalNumber(number -1);
+}
+Console.Write("Натуральные числа в промежутке =>[");
+NaturalNumber(num);
+Console.Write("]");
