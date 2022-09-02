5497a2c7d95c6aaf3d936d210af39ca7df6e7e56
Tamyrlan <tshurenov@gmail.com>
2022-09-02 17:35:22 +0600

    not complete task62

---
+++ task62/Program.cs
@@ -0,0 +1,32 @@
+﻿int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // col
+        {
+            matrix[i, j] = min;
+            min++;
+        }
+
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+
+    }
+}
+int [,] matr = CreateMatrixRndInt(4,4,1,16);
+PrintMatrix(matr);
